a8ee4fc549f85803ac59e58e8c9d84da77e46da7
Nemanja Joksovic <joksovicn@gmail.com>
2012-10-27 03:41:46 +0200

    item crud resource, html files removed

--- app/models/Item.scala
+++ app/models/Item.scala
@@ -4,18 +4,19 @@ import org.joda.time.DateTime
 import reactivemongo.bson._
 import reactivemongo.bson.handlers._
 
-case class Item(
-  id: Option[BSONObjectID],
-  name: String,
-  description: String,
-  publisher: String,
-  creationDate: Option[DateTime],
-  updateDate: Option[DateTime]
-)
+import play.api.libs.json._
+
+case class Item(id: Option[BSONObjectID],
+                name: String,
+                description: String,
+                publisher: String,
+                creationDate: Option[DateTime],
+                updateDate: Option[DateTime])
 
 object Item {
+
   implicit object ItemBSONReader extends BSONReader[Item] {
-    def fromBSON(document: BSONDocument): Item= {
+    def fromBSON(document: BSONDocument): Item = {
       val doc = document.toTraversable
       Item(
         doc.getAs[BSONObjectID]("_id"),
@@ -26,6 +27,11 @@ object Item {
         doc.getAs[BSONDateTime]("updateDate").map(dt => new DateTime(dt.value)))
     }
   }
+
+  implicit object ItemJSONReader extends Reads[Item] {
+    def reads(json: JsValue) = null
+  }
+
   implicit object ItemBSONWriter extends BSONWriter[Item] {
     def toBSON(item: Item) = {
       val bson = BSONDocument(
@@ -33,11 +39,12 @@ object Item {
         "name" -> BSONString(item.name),
         "description" -> BSONString(item.description),
         "publisher" -> BSONString(item.publisher))
-      if(item.creationDate.isDefined)
+      if (item.creationDate.isDefined)
         bson += "creationDate" -> BSONDateTime(item.creationDate.get.getMillis)
-      if(item.updateDate.isDefined)
+      if (item.updateDate.isDefined)
         bson += "updateDate" -> BSONDateTime(item.updateDate.get.getMillis)
       bson
     }
   }
+
 }
